41270c5bedfcd95417c675c2c9664611535b710c
MdQayyumShareef <mdqayyumshareef@gmail.com>
2020-01-09 00:34:47 +0530

    Initial setup with movies list

---
+++ src/app/movie/services/movie.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { movies } from '../model/movie.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MovieService {
+
+    constructor() { }
+
+    getMovies() {
+        return of(movies);
+    }
+}
