25d492581bad8f714e7e7efa7ba63c4946957a3e
MdQayyumShareef <mdqayyumshareef@gmail.com>
2020-01-10 00:43:23 +0530

    add firebase, update .gitignore.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    apiKey: "AIzaSyCBwvX2A49WendbG3Py_6UU5HkisRllTFE",
+    authDomain: "movie-night-27e4f.firebaseapp.com",
+    databaseURL: "https://movie-night-27e4f.firebaseio.com",
+    projectId: "movie-night-27e4f",
+    storageBucket: "movie-night-27e4f.appspot.com",
+    messagingSenderId: "902563795904",
+    appId: "1:902563795904:web:6118b61852467c285f2e25",
+    measurementId: "G-NM11YE6G8N"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    apiKey: "AIzaSyCBwvX2A49WendbG3Py_6UU5HkisRllTFE",
+    authDomain: "movie-night-27e4f.firebaseapp.com",
+    databaseURL: "https://movie-night-27e4f.firebaseio.com",
+    projectId: "movie-night-27e4f",
+    storageBucket: "movie-night-27e4f.appspot.com",
+    messagingSenderId: "902563795904",
+    appId: "1:902563795904:web:6118b61852467c285f2e25",
+    measurementId: "G-NM11YE6G8N"
 };
 
 /*
